1996ba08ecc39bb8291709dcb79c1e51af88dbd7
Lee A. Butler <iraytrace@gmail.com>
2025-04-25 21:16:32 -0400

    update test_example.py for Scatter3DWidget

--- orangecontrib/vis3d/tests/test_example.py
+++ orangecontrib/vis3d/tests/test_example.py
@@ -1,17 +1,17 @@
 import unittest
 from Orange.widgets.tests.base import WidgetTest
 
-from orangecontrib.example.widgets.mywidget import MyWidget
+from orangecontrib.example.widgets.mywidget import Scatter3DWidget
 
 
-class ExampleTests(unittest.TestCase):
+class Vis3dTests(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(1 + 1, 2)
 
 
-class TestMyWidget(WidgetTest):
+class TestScatter3DWidget(WidgetTest):
     def setUp(self):
-        self.widget = self.create_widget(MyWidget)
+        self.widget = self.create_widget(Scatter3DWidget)
 
     def test_addition(self):
         self.assertEqual(1 + 1, 2)
